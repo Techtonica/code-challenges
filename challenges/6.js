7b9a1632140fb72941b013fbf4812f2b2a71ee23
Abigail Edwards <63216164+aedward8@users.noreply.github.com>
2021-02-05 13:32:37 -0800

    change quotations and added a hint
    (1) quotations were not working correctly in the test cases
    (2) I felt that many people were confused with the samples cases and exercise instructions. They thought the letters can be in any order so I added a clarifying hint

--- challenges/6.js
+++ challenges/6.js
@@ -3,20 +3,22 @@
 // Ex
 //
 // ```
-// isRotation(“coding”, “ingcod”) // -> true
+// isRotation("coding", "ingcod") // -> true
 // ```
 //
 // ```
-// isRotation(“help”, “phel”) // -> true
+// isRotation("help", "phel") // -> true
 // ```
 
 // ```
-// isRotation(“github”, “git”)// -> false
+// isRotation("github", "git")// -> false
 // ```
 //
 // ```
-// isRotation(“node”, “edon”) -> false
+// isRotation("node", "edon") -> false
 // ```
+// Hint: Note that the order of the characters of string 2 matters and is actaully a shift in the index of the characters from string 1. 
+// The letters in string 2 cannot be in a randomized order and still pass true.
 //
 // Bonus: find a solution that can work even when the two strings have duplicate letters.
 
